bd248a157c0413664a2bf6780771de2045e1b096
ayleen-gbrl <93256185+ayleen-gbrl@users.noreply.github.com>
2021-11-02 20:59:55 +0100

    Homework_#1

---
+++ code/task-01/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(150);
+  square(150, 130, 150);
+  let red = color(250, 130, 150);
+  fill (red);
+  circle(130, 130, 120);
+  let blue = color(50, 130, 150);
+  fill (blue);
+  triangle(60, 105, 88, 50, 116, 105);
+  let green = color (140, 200, 100);
+  fill (green);
+  line(320, 120, 185, 350);
+  }
